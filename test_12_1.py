173acd61c1a26586715d9b27afe3d6afcd334099
ncidorowa <166540449+ncidorowa@users.noreply.github.com>
2024-07-19 20:26:11 +0400

    Add files via upload

---
+++ test_12_1.py
@@ -0,0 +1,21 @@
+import module12_1
+import unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        for walk in range(11):
+            return walk
+        self.assertEqual(distance, 50)
+
+    def test_run(self):
+        for run in range(11):
+            return run
+        self.assertEqual(distance, 100)
+
+    def test_challenge(self):
+        for walk in range(11):
+            return walk
+        for run in range(11):
+            return run
+        self.assertNotEqual(distance)
+
